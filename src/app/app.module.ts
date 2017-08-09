bd3f825f96580c81eb7aa59f70253ad88c219706
Santiago Schwarz <sschwarzpazos@gmail.com>
2017-08-09 13:17:29 +0200

    feat(*): generate home component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,12 @@ import { HttpModule } from '@angular/http';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
