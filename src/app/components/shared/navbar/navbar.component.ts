a7c6aad9a75b2d05b2a8074f54a78e1f0327c2be
Santiago Schwarz <sschwarzpazos@gmail.com>
2017-08-10 10:30:57 +0200

    feat(*): add brand and routerLinks properties

--- src/app/components/shared/navbar/navbar.component.ts
+++ src/app/components/shared/navbar/navbar.component.ts
@@ -6,6 +6,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
+  public brand:string = 'HeroApppp';
+  routerLinks:any[] = [
+    {
+      link: 'home',
+      title: 'Home'
+    },
+    {
+      link: 'about',
+      title: 'About'
+    },
+    {
+      link: 'heroes',
+      title: 'Heroes'
+    }
+  ];
 
   constructor() { }
 
