f3055ac560c0b99c9827a45d627ae765d87fc1c2
Santiago Schwarz <sschwarzpazos@gmail.com>
2017-08-11 11:58:11 +0200

    feat(ts): create getHeroById method

--- src/app/services/heroes.service.ts
+++ src/app/services/heroes.service.ts
@@ -3,5 +3,24 @@ import axios from 'axios'
 
 @Injectable()
 export class HeroesService {
+  url:string;
+  apikey:string;
+
+  constructor ( ) {
+    // https://gateway.marvel.com:443/v1/public/characters?apikey=b6ec14effa925965bd776af6a56b42d9
+    this.url = 'https://gateway.marvel.com:443/v1/public/characters';
+    this.apikey = 'b6ec14effa925965bd776af6a56b42d9';
+  }
+
+  getHeroById ( id ) {
+    return axios.get(
+      this.url + '/' + id,
+      {
+        params: {
+          apikey: this.apikey
+        }
+      }
+    )
+  }
 
 }
