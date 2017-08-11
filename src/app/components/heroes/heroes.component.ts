2c880f2c80e49d6828fb9fc13d3a54d42d8668a5
Santiago Schwarz <sschwarzpazos@gmail.com>
2017-08-11 13:55:55 +0200

    feat(ts): call Heroes Services

--- src/app/components/heroes/heroes.component.ts
+++ src/app/components/heroes/heroes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HeroesService } from '../../services/heroes.service';
 
 @Component({
   selector: 'app-heroes',
@@ -7,7 +8,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeroesComponent implements OnInit {
 
-  constructor() { }
+  heroes = [];
+
+  constructor( private heroesService: HeroesService ) {
+    /*
+      1009610 => spider-man
+      1009368 => iron-man
+      1009718 => Wolverine
+      1009351 => Hulk
+      1009262 => Daredevil
+      1009629 => Storm
+      1010743 => Groot
+      1009664 => Thor
+      1009465 => Mystique
+    */
+    heroesService
+      .getHeroesById(
+        '1009610 ', '1009368', '1009718',
+        '1009351', '1009262', '1009629',
+        '1010743', '1009664', '1009465' )
+      .then( response  => {
+        this.heroes = response;
+      } );
+  }
 
   ngOnInit() {
   }
